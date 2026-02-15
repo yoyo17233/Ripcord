99655556b73cf4e4fef81f36fcc0c70bd72d2d17
Tim Kwartler <timothy.kwartler@gmail.com>
2026-02-15 09:59:17 -0500

    commit 1 add some files rename some, delete one
    commit description 1

--- install.py
+++ install.py
@@ -4,7 +4,6 @@ import sys
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-# Example usage
 install("discord")
 install("mcrcon")
 install("python-dotenv")
